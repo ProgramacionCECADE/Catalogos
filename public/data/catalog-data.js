79421e6ac928c15783a8eb860fe3e7900dfd2cbd
ProgramacionCECADE <programacion@cecade.org>
2025-11-14 12:07:40 -0600

    First commit

---
+++ public/data/catalog-data.js
@@ -0,0 +1,94 @@
+export const catalogConfig = {
+  businessName: "Demo CECADE",
+  logo: {
+    src: "https://i.ibb.co/xtwSwkyS/FC-VLOGO.png",
+    alt: "Logo de Mi Negocio",
+  },
+  whatsappNumber: "50254394843",
+  products: [
+    {
+      id: 1,
+      name: "Pie de queso",
+      description: "Delicioso pie de queso casero, hecho con ingredientes frescos. Rinde 6 porciones aproximadas. Ideal para postres y celebraciones.",
+      price: "Q15",
+      media: [
+        { 
+          type: "image", 
+          content: "https://i0.wp.com/www.livewellbakeoften.com/wp-content/uploads/2025/03/Cheesecake-Pie-7.jpg?resize=1152%2C1536&ssl=1" 
+        },
+        { 
+          type: "image", 
+          content: "https://i0.wp.com/www.livewellbakeoften.com/wp-content/uploads/2025/03/Cheesecake-Pie-Ingredients.jpg?resize=1152%2C1536&ssl=1" 
+        },
+        { 
+          type: "image", 
+          content: "https://i0.wp.com/www.livewellbakeoften.com/wp-content/uploads/2025/03/Cheesecake-Pie-1.jpg?resize=1152%2C1536&ssl=1" 
+        },
+      ]
+    },
+
+    { 
+      id: 2, 
+      name: "Producto Ejemplo 2", 
+      description: "Otro producto increíble con múltiples características. Puedes añadir toda la información relevante aquí.", 
+      price: "Q45.00", 
+      media: [ 
+        { 
+          type: "image", 
+          content: "https://picsum.photos/id/1025/800/600" 
+        } 
+      ] 
+    },
+
+    { 
+      id: 3, 
+      name: "Producto Ejemplo 3", 
+      description: "Un producto de alta calidad perfecto para tus necesidades. Incluye garantía y envío gratuito.", 
+      price: "Q19.99", 
+      media: [ 
+        { 
+          type: "image", 
+          content: "https://picsum.photos/id/1035/800/600" 
+        } 
+      ] 
+    },
+
+    { 
+      id: 4, 
+      name: "Producto Ejemplo 4", 
+      description: "Producto premium con las mejores características del mercado. Disponible en varios colores.", 
+      price: "Q59.99", 
+      media: [ 
+        { 
+          type: "image", 
+          content: "https://picsum.photos/id/1045/800/600" 
+        } 
+      ] 
+    },
+
+    { id: 5, 
+      name: "Producto Ejemplo 5", 
+      description: "Oferta especial por tiempo limitado. Aprovecha esta oportunidad única.", 
+      price: "Q35.50", 
+      media: [ 
+        { 
+          type: "image", 
+          content: "https://picsum.photos/id/1055/800/600" 
+        } 
+      ] 
+    },
+
+    { 
+      id: 6, 
+      name: "Producto Ejemplo 6", 
+      description: "El producto más vendido de nuestra tienda. Miles de clientes satisfechos.", 
+      price: "Q49.99", 
+      media: [ 
+        { 
+          type: "image", 
+          content: "https://picsum.photos/id/1065/800/600" 
+        } 
+      ] 
+    },
+  ],
+};
